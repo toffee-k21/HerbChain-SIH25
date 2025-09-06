88fe7a21fd5e3c00782385d3b1cadb7313b4fe1c
Taufiq Hassan <taufiq9.2004@gmail.com>
2025-09-07 00:02:23 +0530

    config fix

--- herbchain-collector/next.config.js
+++ herbchain-collector/next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === 'development',
+});
 
 const nextConfig = {
   reactStrictMode: true,
   // other Next.js config
 };
 
-module.exports = withPWA(nextConfig, {
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  // disable: process.env.NODE_ENV === 'development',
-});
+module.exports = withPWA(nextConfig);
